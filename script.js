2821cd8777925d8fcf46f91c9ab03f2882a00b70
Susie Douang <soutsada.d@gmail.com>
2024-04-01 23:11:04 -0500

    calculate AP and HP and test values

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let fungusHp = 100;
 let myAp = 100;
+
 let fungusDamage = 0;
 let myDamage = 0;
 
@@ -10,18 +11,41 @@ function onReady() {
   let hpMeter = document.getElementById('hp-meter').value;
   console.log(hpMeter);
 }
-
 onReady();
 
 function attackArcane() {
   console.log('attackArcane');
+
+  myAp = myAp - 14;
+  fungusHp = fungusHp - 14;
+
+  console.log(fungusHp);
+  console.log(myAp);
 }
 function attackEntangle() {
   console.log('attackEntangle');
+
+  myAp = myAp - 23;
+  fungusHp = fungusHp - 9;
+
+  console.log(fungusHp);
+  console.log(myAp);
 }
 function attackDragon() {
   console.log('attackDragon');
+
+  myAp = myAp - 38;
+  fungusHp = fungusHp - 47;
+
+  console.log(fungusHp);
+  console.log(myAp);
 }
 function attackStar() {
   console.log('attackStar');
+
+  myAp = myAp - 33;
+  fungusHp = fungusHp - 25;
+
+  console.log(fungusHp);
+  console.log(myAp);
 }
